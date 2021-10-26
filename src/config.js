ddd00f5739430494975ec1e4c3d442649a21cf69
Heimdall <74313913+ichbinheimdall@users.noreply.github.com>
2021-10-26 16:56:44 +0300

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,13 @@
+const config = { 
+//------------Connections-----------//
+    Client_Token: '***REMOVED***',
+    MongoDB_ConnectURL: '***REMOVED***/database?retryWrites=true&w=majority',
+    //------------BotSettings-----------//
+    Prefix: '.',
+    BotOwners: ["387675598044135436"],
+    Custom_Status_Text: 'hey, işleri yoluna koyunca hizmet vermeye devam edeceğim.',
+    Custom_Status_Type: 'PLAYING', // => PLAYING / WATCHING / LISTENING
+    Custom_Status: 'idle' // => dnd / idle / online / invisible
+};
+  
+module.exports = config;
