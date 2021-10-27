272f9a89348de5e5215230fa7fe7c60f07d154ab
Heimdall <74313913+ichbinheimdall@users.noreply.github.com>
2021-10-27 23:19:49 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const config = {
     //------------BotSettings-----------//
     Prefix: '.',
     BotOwners: ["387675598044135436"],
-    Custom_Status_Text: 'hey, işleri yoluna koyunca hizmet vermeye devam edeceğim.',
+    Custom_Status_Text: ".davet | .yardım",
     Custom_Status_Type: 'PLAYING', // => PLAYING / WATCHING / LISTENING
     Custom_Status: 'idle' // => dnd / idle / online / invisible
 };
