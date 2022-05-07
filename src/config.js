cc6d2fc2de76b9273fe6665a2eeceef393bcd03d
Heimdall <74313913+ichbinheimdall@users.noreply.github.com>
2022-05-07 18:26:20 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const config = {
     MongoDB_ConnectURL: '***REMOVED***/database?retryWrites=true&w=majority',
     //------------BotSettings-----------//
     Prefix: '.',
-    BotOwners: ["387675598044135436"],
+    BotOwners: ["697494349856112702"],
     Custom_Status_Text: ".oy | .yardım",
     Custom_Status_Type: 'WATCHING', // => PLAYING / WATCHING / LISTENING
     Custom_Status: 'idle' // => dnd / idle / online / invisible
