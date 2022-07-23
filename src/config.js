cb1d25114c3edeafaababcb782e3a8fc1795fa2c
Heimdall <74313913+ichbinheimdall@users.noreply.github.com>
2022-07-23 18:49:24 +0300

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,6 @@
+module.exports = { 
+        Client_Token: "***REMOVED***",
+        MongoDB_ConnectURL: "***REMOVED***/database?retryWrites=true&w=majority",
+        BotOwners: ["387675598044135436"],
+        BotStatus: "Chat Guard Yenilendi! Şimdi v14 ve slash komutlu! | /davet /yardım"
+};
