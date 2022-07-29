74609827a6be73e6559a475ff3c7d1e9dd536c36
Heimdall <74313913+ichbinheimdall@users.noreply.github.com>
2022-07-29 17:16:11 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
         Client_Token: "***REMOVED***",
         MongoDB_ConnectURL: "***REMOVED***/database?retryWrites=true&w=majority",
         BotOwners: ["387675598044135436"],
-        BotStatus: "Chat Guard Yenilendi! Şimdi v14 ve slash komutlu! | /davet /yardım"
+        BotStatus: "Chat Guard Yenilendi! | /davet /yardım"
 };
