dcb4e783d876a5363358cf55ef4a502bcb916846
Heimdall <umutguden99@gmail.com>
2022-11-22 13:46:06 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
         Client_Token: "***REMOVED***",
         MongoDB_ConnectURL: "***REMOVED***/database?retryWrites=true&w=majority",
         BotOwners: ["387675598044135436"],
-        BotStatus: "/yardım | /davet | www.SmmPres.com ayrıcalığıyla.."
+        BotStatus: "/yardım | /davet | SmmPres.com"
 };
