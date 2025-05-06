c56b70c475706173b576d172d8ac96e9fd6a49cc
Heimdall <umutguden99@gmail.com>
2025-05-06 22:10:44 +0300

    Guidance

--- src/config.js
+++ src/config.js
@@ -1,6 +1,10 @@
 module.exports = { 
-        Client_Token: "***REMOVED***",
-        MongoDB_ConnectURL: "***REMOVED***/database?retryWrites=true&w=majority",
-        BotOwners: ["387675598044135436"],
-        BotStatus: "/help | /invite"
+        Client_Token: "CLIENT_TOKEN",
+        MongoDB_ConnectURL: "MONGODB_CONNECTION_STRING",
+        BotOwners: ["OWNER_ID"],
+        BotStatus: "Bot is online!",
 };
+// Replace CLIENT_TOKEN with your bot token
+// Replace MONGODB_CONNECTION_STRING with your MongoDB connection string
+// Replace OWNER_ID with your Discord user ID (as a string)
+// Replace Bot is online! with your desired bot status message
